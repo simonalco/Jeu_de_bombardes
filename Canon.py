ea57bc7a986a6d43c00819941db0c8ff7d135a82
simonalco <simon.alphonse-cormier.1@ulaval.ca>
2022-12-14 21:13:24 -0500

    Initial commit

---
+++ Canon.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from math import pi, sin, cos
+
+class Canon():
+    def __init__(self, boss, x, y):
+        self.boss = boss
+        pass
+
+    def orienter(self, angle):
+        pass
+
+
+if __name__ == '__name__':
+    pass
---
+++ README.md
@@ -0,0 +1,2 @@
+# Jeu_de_bombardes
+ Jeu de bombardes du manuel de Gérard Swinnen
